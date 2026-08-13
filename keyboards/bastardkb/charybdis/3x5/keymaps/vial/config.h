288911723389a34e0428879cc62f4435788e575d
MisterOlivka <misterolivkaii@tutamail.com>
2026-08-13 15:08:50 +0300

    Update config.h

--- keyboards/bastardkb/charybdis/3x5/keymaps/vial/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/vial/config.h
@@ -31,13 +31,15 @@
 #define USB_SUSPEND_WAKEUP_DELAY 1000
 
 // --- Фикс "левая половина не работает после включения ПК, помогает только переподключение USB" ---
-// Мастер никогда не считает слейв "отключённым" и продолжает пытаться связаться с ним каждый скан-цикл,
-// а не раз в 500мс после серии неудач
-#define SPLIT_MAX_CONNECTION_ERRORS 0
+// Мастер быстрее помечает соединение "разорванным" (после 3 неудачных попыток вместо 10 по умолчанию)
+// и чаще пробует восстановить связь
+#define SPLIT_MAX_CONNECTION_ERRORS 3
+#define SPLIT_CONNECTION_CHECK_TIMEOUT 250
 // Слейв (левая половина) сам себя перезагружает, если не слышит мастера дольше SPLIT_WATCHDOG_TIMEOUT мс -
-// это устраняет "залипание" связи при холодном старте без необходимости дёргать USB-кабель
+// это устраняет "залипание" связи при холодном старте без необходимости дёргать USB-кабель.
+// Таймаут watchdog обязан быть больше SPLIT_USB_TIMEOUT (2000 выше)
 #define SPLIT_WATCHDOG_ENABLE
-#define SPLIT_WATCHDOG_TIMEOUT 1500
+#define SPLIT_WATCHDOG_TIMEOUT 3000
 
 #define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
